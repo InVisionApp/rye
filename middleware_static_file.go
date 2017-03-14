67fa4b17fd5f8569ab52febd7ad1e6439fed916f
Cale Hoopes <calehoopes@invisionapp.com>
2017-03-13 17:47:32 -0700

    Fixing some things

--- middleware_static_file.go
+++ middleware_static_file.go
@@ -9,9 +9,18 @@ type staticFile struct {
 }
 
 /*
-NewStaticFile creates a new handler to serve a file from a path on the local filesystem. The path should be an absolute path -> i.e., it's up to the program using Rye to correctly determine what path it should be serving from. An example is available in the `static_example.go` file which shows setting up a path relative to the go executable.
+NewStaticFile creates a new handler to serve a file from a path on the local filesystem.
+The path should be an absolute path -> i.e., it's up to the program using Rye to
+correctly determine what path it should be serving from. An example is available
+in the `static_example.go` file which shows setting up a path relative to
+the go executable.
 
-The purpose of this handler is to serve a specific file for any requests through the route handler. For instance, in the example below, any requests made to `/ui` will always be routed to /dist/index.html. This is important for single page applications which happen to use client-side routers. Therefore, you might have a webpack application with it's entrypoint `/dist/index.html`. That file may point at your `bundle.js`. Every request into the app will need to always be routed to `/dist/index.html`
+The purpose of this handler is to serve a specific file for any requests through the
+route handler. For instance, in the example below, any requests made to `/ui` will
+always be routed to /dist/index.html. This is important for single page applications
+which happen to use client-side routers. Therefore, you might have a webpack application
+with it's entrypoint `/dist/index.html`. That file may point at your `bundle.js`.
+Every request into the app will need to always be routed to `/dist/index.html`
 
 Example use case:
 
--- middleware_static_filesystem.go
+++ middleware_static_filesystem.go
@@ -10,8 +10,16 @@ type staticFilesystem struct {
 }
 
 /*
-NewStaticFilesystem creates a new handler to serve a filesystem from a path on the local filesystem. The path should be an absolute path -> i.e., it's up to the program using Rye to correctly determine what path it should be serving from. An example is available in the `static_example.go` file which shows setting up a path relative to the go executable.
-The primary benefit of this is to serve an entire set of files. You can pre-pend typical Rye middlewares to the chain. The static filesystem middleware should always be last in a chain, however. The `stripPrefix` allows you to ignore the prefix on requests so that the proper files will be matched.
+NewStaticFilesystem creates a new handler to serve a filesystem from a path
+on the local filesystem. The path should be an absolute path -> i.e., it's
+up to the program using Rye to correctly determine what path it should be
+serving from. An example is available in the `static_example.go` file which
+shows setting up a path relative to the go executable.
+
+The primary benefit of this is to serve an entire set of files. You can
+pre-pend typical Rye middlewares to the chain. The static filesystem
+middleware should always be last in a chain, however. The `stripPrefix` allows
+you to ignore the prefix on requests so that the proper files will be matched.
 
 Example use case:
 
